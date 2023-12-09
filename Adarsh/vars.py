b57066aa2a8000dcb06f64304c178409fb5c6f70
Heypatell <95734566+manupatell@users.noreply.github.com>
2023-12-09 12:45:19 +0530

    Add files via upload

---
+++ Adarsh/vars.py
@@ -0,0 +1,40 @@
+# (c) adarsh-goel
+import os
+from os import getenv, environ
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+class Var(object):
+    MULTI_CLIENT = False
+    API_ID = int(getenv('API_ID', '29775772'))
+    API_HASH = str(getenv('API_HASH', '6019769d5692d17c992d226061055dac'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6807386062:AAGb-9Itcpwry7P7OthXRHReh8sDIRVXrXw'))
+    name = str(getenv('name', 'filetolinkmulti'))
+    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(getenv('WORKERS', '4'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002033286207'))
+    PORT = int(getenv('PORT', 8080))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6305575094").split())  
+    NO_PORT = bool(getenv('NO_PORT', False))
+    APP_NAME = str(getenv('APP_NAME'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'MrNamee'))
+    if 'DYNO' in environ:
+        ON_HEROKU = True
+        APP_NAME = str(getenv('APP_NAME'))
+    
+    else:
+        ON_HEROKU = False
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL',False))
+    if HAS_SSL:
+        URL = "https://{}/".format(FQDN)
+    else:
+        URL = "https://{}/".format(FQDN)
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://manu:manu@manu.tcahodt.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Manubots'))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
